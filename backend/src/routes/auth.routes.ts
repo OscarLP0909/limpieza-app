2e77935ac8bcbf9cd64186b862a55d8669386a7c
OscarLP09 <oscar.s352656@cesurformacion.com>
2026-04-08 12:16:19 +0200

    Register de Cliente hecho

--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { login, logout, me } from "../controllers/auth.controller";
+import { login, logout, me, register } from "../controllers/auth.controller";
 import { authenticateToken } from "../middlewares/auth.middleware";
 
 const router = Router();
 
+router.post('/register', register);
 router.post('/login', login);
 router.post('/logout', authenticateToken, logout);
 router.get('/me', authenticateToken, me);
