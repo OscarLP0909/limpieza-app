3eb20eb4e5067e94543afb8f2305fb13d443f1d7
OscarLP09 <oscar.s352656@cesurformacion.com>
2026-04-08 17:02:30 +0200

    Endpoint Tipos_Servicio

--- backend/src/routes/services.routes.ts
+++ backend/src/routes/services.routes.ts
@@ -1,10 +1,15 @@
 import { Router } from "express";
 import { authenticateToken, authorizeRoles } from "../middlewares/auth.middleware";
-import { getServices } from "../controllers/services.controller";
+import { getServices, createService, updateService, deleteService } from "../controllers/services.controller";
 
 
 const router = Router();
 
 router.get('/', authenticateToken, authorizeRoles('admin'), getServices);
+router.post('/', authenticateToken, authorizeRoles('admin'), createService);
+router.delete('/:id', authenticateToken, authorizeRoles('admin'), deleteService);
+router.patch('/:id', authenticateToken, authorizeRoles('admin'), updateService);
+
+
 
 export default router;
