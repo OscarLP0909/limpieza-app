62fbd4c737071eb394e6a46bcc5c849c96194ddf
OscarLP09 <oscar.s352656@cesurformacion.com>
2026-04-09 13:13:44 +0200

    CRUD Employees

--- backend/src/routes/employees.routes.ts
+++ backend/src/routes/employees.routes.ts
@@ -1,11 +1,15 @@
 import { Router } from "express";
 import { authenticateToken, authorizeRoles } from "../middlewares/auth.middleware";
+import { getEmployees, createEmployee, updateEmployee, deleteEmployee } from "../controllers/employees.controller";
 
 
 
 const router = Router();
 
-
+router.get('/', authenticateToken, authorizeRoles('admin', 'gestor'), getEmployees);
+router.post('/', authenticateToken, authorizeRoles('admin'), createEmployee);
+router.patch('/:id', authenticateToken, authorizeRoles('admin'), updateEmployee);
+router.delete('/:id', authenticateToken, authorizeRoles('admin'), deleteEmployee);
 
 
 export default router;
